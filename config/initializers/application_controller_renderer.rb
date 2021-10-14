bd511893034e7d2d30ed3a4797e9aed983f9b2cb
kitayama3352 <kitayama3352@gmail.com>
2021-10-14 14:09:25 +0000

    7章【画像投稿機能・バリデーションの設定】 END

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ac35527a31c38eee03f64f7be728bb6ec08fb2d8ddc7848c9700ed85665214c893ddbf5c8f4cd06704f05e4bee9e8aa9187b3c6752af71f0268b059a1c2cd302'
